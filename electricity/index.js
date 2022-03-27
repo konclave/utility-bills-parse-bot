2b9f42dba42bfc69fcb418fccdbe5c5b45f6fbee
Ivan Vasilev <vianomore@gmail.com>
2022-03-27 13:47:37 +0200

    add fetch electricity from S3

--- electricity/index.js
+++ electricity/index.js
@@ -1,10 +1,12 @@
 import { fetch as fetchElectricity } from './fetch-electricity.js';
 import { parse } from './parse-electricity.js';
 import { getErrorMessage } from '../shared/message.js';
+import { fetchFromS3 } from './fetch-s3.js';
 
 export async function fetch() {
   try {
-    const pdf = await fetchElectricity();
+    // const pdf = await fetchElectricity();
+    const pdf = await fetchFromS3();
     return await parse(pdf);
   } catch(error) {
     return { text: getErrorMessage('⚡️'), error };

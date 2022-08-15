54638209eeee748cedf0f4358072867bb37831f3
Ivan Vasilev <vianomore@gmail.com>
2022-08-15 12:40:49 +0200

    feat: refactor pdf storage

--- shared/s3.js
+++ shared/s3.js
@@ -1,4 +1,10 @@
-import { S3Client, PutObjectCommand, ListObjectsV2Command, DeleteObjectsCommand, GetObjectCommand  } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  PutObjectCommand,
+  ListObjectsV2Command,
+  DeleteObjectsCommand,
+  GetObjectCommand,
+} from '@aws-sdk/client-s3';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -23,8 +29,10 @@ export async function fetch(filename) {
   const bucketName = process.env['YC_S3_BUCKET'];
   const s3Client = getS3Client();
 
-  const listResponse = await s3Client.send(new ListObjectsV2Command({ Bucket: bucketName}));
-  const hasFile = listResponse.Contents?.some(({ Key}) => Key === filename);
+  const listResponse = await s3Client.send(
+    new ListObjectsV2Command({ Bucket: bucketName })
+  );
+  const hasFile = listResponse.Contents?.some(({ Key }) => Key === filename);
 
   if (hasFile) {
     const params = {
@@ -46,7 +54,6 @@ export async function fetch(filename) {
   return Buffer.concat([]);
 }
 
-
 export async function store(pdf, filename) {
   const s3Client = getS3Client();
 
@@ -59,7 +66,7 @@ export async function store(pdf, filename) {
   try {
     return await s3Client.send(new PutObjectCommand(params));
   } catch (err) {
-    console.log("Error", err);
+    console.log('Error', err);
   }
 }
 
@@ -67,17 +74,48 @@ export async function purge(predicate) {
   const s3Client = getS3Client();
   const params = {
     Bucket: bucketName,
-  }
+  };
   const data = await s3Client.send(new ListObjectsV2Command(params));
-  const objects = data.Contents?.map(({ Key}) => ({ Key }));
+  const objects = data.Contents?.map(({ Key }) => ({ Key }));
 
   if (objects?.length) {
     const options = {
       Bucket: bucketName,
       Delete: {
-        Objects: objects.filter(predicate)
-      }
+        Objects: objects.filter(predicate),
+      },
     };
     await s3Client.send(new DeleteObjectsCommand(options));
   }
+}
+
+function getFilenamesToKeep(filename, prefixes) {
+  const chunks = filename.split('.')[0].split('-');
+  const year = Number(chunks[chunks.length - 1]);
+  const month = Number(chunks[chunks.length - 2]);
+
+  const keep = [];
+
+  for (let i = 0; i < KEEP_INVOICES_NUMBER; i++) {
+    let prevMonth = month - i;
+    let prevYear = year;
+    if (prevMonth < 1) {
+      prevMonth += 12;
+      prevYear -= 1;
+    }
+
+    for (let prefix of prefixes) {
+      keep.push(
+        `${prefix}${String(prevMonth).padStart(2, '0')}-${prevYear}.pdf`
+      );
+    }
+  }
+
+  return keep;
+}
+
+export async function purgeStorage(filename, prefixes) {
+  const keep = getFilenamesToKeep(filename, prefixes);
+  const predicate = (object) => !keep.includes(object['Key']);
+  return S3.purge(predicate);
 }

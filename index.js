d311bb819384e24c17ea786e56443ca234e9d392
Ivan Vasilev <vianomore@gmail.com>
2022-12-15 20:37:24 +0100

    chore: improve logging

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ import { webhookCallback } from './mosenergo-bill-store/index.js';
 dotenv.config();
 
 if (process.env.NODE_ENV === 'development') {
-  webhookCallback({
-    body: JSON.stringify({
-      invoicelink_url:
-        'https://some.url?args=http://localhost:8000/water-bill.pdf',
-    }),
-  });
+  // webhookCallback({
+  //   body: JSON.stringify({
+  //     invoicelink_url:
+  //       'https://some.url?args=http://localhost:8000/water-bill.pdf',
+  //   }),
+  // });
+  const messages = await bot.getValues();
 }
 
 export const handler = async function (event) {

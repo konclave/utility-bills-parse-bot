a24d0c901b97c4a1dc27315a01161389d67a63f6
Ivan Vasilev <vianomore@gmail.com>
2024-10-13 22:13:12 +0200

    test: remove esmock dep, use node:test mock

--- src/water/__test__/parse-water.test.js
+++ src/water/__test__/parse-water.test.js
@@ -1,26 +1,34 @@
-import { describe, it, mock, beforeEach } from 'node:test';
+import { describe, it, mock, before } from 'node:test';
+import { resolve } from 'node:path'
 import assert from 'node:assert/strict';
-import esmock from 'esmock';
 import { pdfStringsMock } from './pdf-strings.mock.js';
 
-const { parse } = await esmock('../parse-water.js', {
-  '../../shared/parse-pdf.js': {
-    getStringsFromPdf: async () => pdfStringsMock,
-  },
-  '../fetch-water.js': {
-    filenamePrefix: 'water-',
-  },
-});
+describe('parse', async (t) => {
+  let parse;
 
-describe('parse', async () => {
-  const binary = [1, 0, 1, 0];
-  // expected result based on the pdfStringsMock
-  const expected = {
-    text: '💧: 3518.51 ₽\n(320.00 + 1378.71 + 720.00 + 1099.80)',
-    value: 3518.51,
-    fileTitle: 'water-bill.pdf',
-    fileBuffer: binary,
-  };
+  let binary;
+  let expected;
+
+  before(async () => {
+    mock.module(resolve(import.meta.dirname, '../../shared/parse-pdf.js'), {
+      namedExports: { getStringsFromPdf: async () => pdfStringsMock },
+    });
+
+    mock.module(resolve(import.meta.dirname, '../fetch-water.js'), { 
+      namedExports: { filenamePrefix: 'water-' },
+    });
+
+    binary = [1, 0, 1, 0];
+    // expected result based on the pdfStringsMock
+    expected = {
+      text: '💧: 3518.51 ₽\n(320.00 + 1378.71 + 720.00 + 1099.80)',
+      value: 3518.51,
+      fileTitle: 'water-bill.pdf',
+      fileBuffer: binary,
+    };
+
+    ({ parse } = await import('../parse-water.js'));
+  });
 
   it('should return an object with "text", "value", "fileTitle", and "fileBuffer" properties when given a non-empty binary input', async () => {
     const result = await parse(binary);

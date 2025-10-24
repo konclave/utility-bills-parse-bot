5665ae7b34b239d2bcde6e3d0be337b689f571db
Ivan Vasilev <vianomore@gmail.com>
2025-10-24 13:02:15 +0200

    feat: choose venue option

--- src/bot/processing.js
+++ src/bot/processing.js
@@ -4,8 +4,18 @@ import * as mosobleirc from '../mosobleirc/index.js';
 import { getTotal } from '../shared/calculations.js';
 import { getPeriodString } from '../shared/period.js';
 
-export async function getValues({ processMessage, handleError }) {
-  const billPromises = [water.fetch(), electricity.fetch(), mosobleirc.fetch()];
+export async function getValues({ processMessage, handleError, venue }) {
+  let billPromises;
+  switch (venue) {
+    case 'O':
+      billPromises = [mosobleirc.fetch()];
+      break;
+    case 'T':
+      billPromises = [water.fetch(), electricity.fetch()];
+      break;
+    default:
+      billPromises = [water.fetch(), electricity.fetch(), mosobleirc.fetch()];
+  }
 
   const withHandlers = billPromises.map(async (promise) =>
     promise.then(processMessage).catch(handleError),
--- src/mosobleirc/index.js
+++ src/mosobleirc/index.js
@@ -4,12 +4,22 @@ import { getErrorMessage } from '../shared/message.js';
 import { getTodayISODate } from '../shared/period.js';
 
 export async function fetch() {
+  // if the time is between 20:00 UTC and 03:00 UTC, reply with a message "Одинцово: данные доступны только в период с 03:00 до 20:00 UTC"
+
+  const now = new Date();
+  const hour = now.getUTCHours();
+
+  if (hour >= 20 || hour < 3) {
+    return {
+      text: 'Одинцово: данные доступны только в период с 03:00 до 20:00 UTC',
+    };
+  }
+
   try {
     const date = getTodayISODate();
     const json = await fetchCharges(date);
     return parseCharges(json);
   } catch (error) {
-    console.log(JSON.stringify({ origin: 'Одинцово', error: error.message }));
     return { text: getErrorMessage('Одинцово'), error: error.message };
   }
 }

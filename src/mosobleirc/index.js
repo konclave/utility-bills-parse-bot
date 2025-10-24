1d4ff0330d786727ea3e0a948c4a683b32add5e2
Ivan Vasilev <vianomore@gmail.com>
2025-10-24 16:26:02 +0200

    feat: store mosobleirc fetched values in json file

--- src/mosobleirc/index.js
+++ src/mosobleirc/index.js
@@ -2,6 +2,7 @@ import { fetchCharges } from './fetch.js';
 import { parseCharges } from './parse.js';
 import { getErrorMessage } from '../shared/message.js';
 import { getTodayISODate } from '../shared/period.js';
+import * as storage from './store.js';
 
 export async function fetch() {
   // if the time is between 20:00 UTC and 03:00 UTC, reply with a message "Одинцово: данные доступны только в период с 03:00 до 20:00 UTC"
@@ -17,8 +18,14 @@ export async function fetch() {
 
   try {
     const date = getTodayISODate();
+    const fromStore = storage.fetch(date);
+    if (fromStore) {
+      return fromStore;
+    }
     const json = await fetchCharges(date);
-    return parseCharges(json);
+    const parsed = parseCharges(json);
+    await storage.store(date, parsed);
+    return parsed;
   } catch (error) {
     return { text: getErrorMessage('Одинцово'), error: error.message };
   }

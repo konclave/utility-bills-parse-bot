2fc45233112b17db1423190f6e274034df01ccd5
Ivan Vasilev <vianomore@gmail.com>
2025-10-24 16:26:02 +0200

    feat: store mosobleirc fetched values in json file

---
+++ src/mosobleirc/store.js
@@ -0,0 +1,22 @@
+import * as s3 from '../shared/s3.js';
+
+const MOSOBL_STORAGE_FILENAME = 'mosobleirc.json';
+
+export async function store(period, record) {
+  const fileBuffer = await s3.fetch(MOSOBL_STORAGE_FILENAME);
+  if (!fileBuffer) {
+    return;
+  }
+  const values = JSON.parse(fileBuffer.toString());
+  values[period] = record;
+  await s3.store(JSON.stringify(values), MOSOBL_STORAGE_FILENAME);
+}
+
+export async function fetch(period) {
+  const fileBuffer = await s3.fetch(MOSOBL_STORAGE_FILENAME);
+  if (!fileBuffer) {
+    return;
+  }
+  const values = JSON.parse(fileBuffer.toString());
+  return values[period];
+}
